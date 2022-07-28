a7c87192abb7e73f1f963619195760d31c36c66b
Tornak9 <Tornak9@gmail.com>
2022-07-28 04:51:01 +0200

    Add game function, which limits games to 5 and tracks score

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function getRandomComputerChoice() {
     }
 }
 
-console.log(getRandomComputerChoice());
-
 function playerSelection() {
     let playerSelection = prompt("Pick between rock, paper and scissors!");
     playerSelection = playerSelection.toLowerCase();
@@ -48,5 +46,23 @@ function playtrough(playerselect, computerselect) {
     }
 };
 
-function game()
-console.log(playtrough(playerSelection(), getRandomComputerChoice()));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let gameScore = playtrough(playerSelection(), getRandomComputerChoice());
+        if (gameScore === "Yes, you got him good!") {
+            playerScore++;
+        } else if (gameScore === "Oh no, you lost!") {
+            computerScore++;
+        }
+    }
+    if (playerScore > computerScore) {
+        return `Congratulations, you've won ${playerScore} to ${computerScore}!`;
+    } else if (playerScore === computerScore) {
+        return `This game was a draw!`;
+    } else if (playerScore < computerScore) {
+        return `You lost ${computerScore} to ${playerScore}! But don't give up, you'll get him next time!`
+    }
+}
+console.log(game());
